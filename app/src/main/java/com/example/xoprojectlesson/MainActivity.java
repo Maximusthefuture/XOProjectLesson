dab25142a19d9e8880db9221bd9a07267eb6dcab
maximus <naturemade@yandex.ru>
2019-12-24 12:35:23 +0300

    Доработка

--- app/src/main/java/com/example/xoprojectlesson/MainActivity.java
+++ app/src/main/java/com/example/xoprojectlesson/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.xoprojectlesson;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
-import java.util.logging.LogManager;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements XOCustomVIew.WinListener {
     private static final String TAG =  "MainActivity";
-    private GridLayout gridLayout;
-    private boolean player1Turn = true;
-    TicTacToeField ticTacToeField;
+
+    private TicTacToeField ticTacToeField;
+    private XOCustomVIew mXOCustomVIew;
+    private TextView circleWin;
+    private TextView xWin;
+    private View result;
+    private Button restart;
+    private int circleScore = 0;
+    private int xScore = 0;
+    private TextView winnerText;
 
 
 //    GestureDetector;
@@ -25,9 +27,65 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        init();
+
+    }
+
+    private void init() {
+        mXOCustomVIew = findViewById(R.id.xo_custom_view);
+        circleWin = findViewById(R.id.circle_scores);
+        xWin = findViewById(R.id.x_scores);
+        restart = findViewById(R.id.button_restart);
+        result = findViewById(R.id.restart_layout);
+        winnerText = findViewById(R.id.winner_text_view);
+
+        restart.setOnClickListener(v -> {
+            restart.setVisibility(View.INVISIBLE);
+            mXOCustomVIew.restart();
+            winnerText.setVisibility(View.INVISIBLE);
+        });
+        mXOCustomVIew.setOnWinListener(this);
 
     }
 
 
+    public void showScores(TicTacToeField.Figure scoreSide) {
+
+
+        result.setVisibility(View.VISIBLE);
+
+        switch (scoreSide) {
+            case CIRCLE:
+                winnerText.setText(R.string.circle_text);
+                break;
+            case CROSS:
+                winnerText.setText(R.string.cross_text);
+                break;
+            case NONE:
+                winnerText.setText(R.string.draw_figure_text);
+                break;
+
+
+        }
+        showScores();
+    }
+
+    private void showScores() {
+        circleWin.setText(Integer.toString(circleScore));
+        xWin.setText(Integer.toString(xScore));
+    }
+
+
+    @Override
+    public void onWin(TicTacToeField.Figure win) {
+        if (win == TicTacToeField.Figure.CIRCLE) {
+            circleScore++;
+        } else {
+            xScore++;
+        }
+
+        showScores(win);
+    }
 }
--- app/src/main/java/com/example/xoprojectlesson/XOCustomVIew.java
+++ app/src/main/java/com/example/xoprojectlesson/XOCustomVIew.java
@@ -5,146 +5,296 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PointF;
-import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class XOCustomVIew extends View {
 
+    private static final float STROKE = 10f;
+    private static final String TAG = "XOCustomView";
+
     private Paint gridPaint = new Paint();
-    int width;
-    int height;
-    int alternativeWidth;
-    int alternativeHeight;
-    float tolzina = 5;
-    float xRatio = 1 / 3f;
+    private float xRatio = 1 / 3f;
     private Path mPath = new Path();
     private Paint mPaint = new Paint();
+    private TicTacToeField mTicTacToeField;
+    int COUNT = 3;
+    private Drawable circle;
+    private Drawable cross;
+    private int row = 0;
+    private int col = 0;
+    private int cells = COUNT;
+    private int size = 0;
+    private int left = 0;
+    private int right = 0;
+    private int top = 0;
+    private int bottom = 0;
+    private boolean[][] winnerMatrix;
+    private WinListener mWinListener = new WinListenerClass();
+    private GameStatus mGameStatus = GameStatus.GAME;
 
+    private TicTacToeField.Figure mFigure = TicTacToeField.Figure.NONE;
 
 
     public XOCustomVIew(Context context) {
         super(context);
+        init(context);
+
     }
 
     public XOCustomVIew(Context context, AttributeSet attrs) {
         super(context, attrs);
         initPaint();
+        init(context);
     }
 
     public XOCustomVIew(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
-    public XOCustomVIew(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-
-    private void initPaint() {
-        mPaint.setColor(Color.RED);
+    private void initPaint()  {
+        mPaint.setColor(Color.BLACK);
         mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(10f);
+        mPaint.setStrokeWidth(STROKE);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeJoin(Paint.Join.MITER);
+        gridPaint.setColor(Color.BLACK);
+        gridPaint.setStrokeWidth(STROKE);
+        mTicTacToeField = new TicTacToeField(cells);
+
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-//        drawGrid(canvas);
-        drawHorizontalLine(canvas);
-        drawVerticalLine(canvas);
-        drawX(canvas);
-        canvas.drawPath(mPath, mPaint);
 
+        invalidate();
+        switch (mGameStatus){
+            case START:
+                drawHorizontalLine(canvas);
+                drawVerticalLine(canvas);
+                circle.setBounds(getLeft(), getTop(), getRight(),  getBottom());
+                canvas.drawPath(mPath, mPaint);
+                drawFigures(canvas);
+            case GAME_OVER:
+                drawHorizontalLine(canvas);
+                drawVerticalLine(canvas);
+            case GAME:
+            case RESTART:
+
+
+        }
+    }
+
+    public void restart() {
+        mGameStatus = GameStatus.START;
+        mTicTacToeField = new TicTacToeField(cells);
 
     }
 
+    private void init(Context context) {
 
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        float eventX = event.getX();
-//        float eventY = event.getY();
-//
-//        switch (event.getAction()) {
-//            case MotionEvent.ACTION_DOWN:
-//                mPath.moveTo(eventX, eventY);
-//                return true;
-//            case MotionEvent.ACTION_MOVE:
-//                mPath.lineTo(eventX, eventY);
-//                invalidate();
-//                return true;
-//
-//            default:
-//                return super.onTouchEvent(event);
-//
-//        }
+        mGameStatus = GameStatus.START;
+        circle = context.getDrawable(R.drawable.circle);
+        cross = context.getDrawable(R.drawable.cross);
 
+    }
 
+    public interface WinListener {
+        void onWin(TicTacToeField.Figure win);
+    }
+
+    public class WinListenerClass implements WinListener {
+
+        @Override
+        public void onWin(TicTacToeField.Figure win) {
+
+        }
+    }
+
+    public void setOnWinListener(WinListener listener) {
+        if (listener != null) {
+            mWinListener = listener;
+        } else {
+            mWinListener = new WinListenerClass();
+        }
+    }
+
+    private void drawFigures(Canvas canvas) {
+        int cellSize = (int) (size - STROKE * (cells - 1) - (STROKE * 2)) / cells;
+        Drawable drawable;
+        for (int row = 0; row < cells; row++) {
+            for (int col = 0; col < cells; col++) {
+                int x = (int) (left + STROKE / 2+ (STROKE * col) + (cellSize * col));
+                int y = (int) (top + STROKE / 2 + (STROKE * row) + (cellSize * row));
+
+                if (!mTicTacToeField.isEmptyCell(row, col)) {
+                    if (mTicTacToeField.getFigure(row, col) == TicTacToeField.Figure.CROSS) {
+                        drawable = cross;
+                    } else {
+                        drawable = circle;
+                    }
+//
+                    drawable.setBounds(x, y, x + cellSize, y + cellSize);
+                    drawable.draw(canvas);
+                }
+            }
+        }
+}
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float eventX = event.getX();
-        float eventY = event.getY();
-
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                int cellSize = size / cells;
+                int x = (int) (event.getX() - left);
+                int y = (int) (event.getY() - top);
 
-                return true;
+                int mRow = y / cellSize;
+                int mCol = x / cellSize;
 
-            case MotionEvent.ACTION_MOVE:
-                mPath.lineTo(eventX, eventY);
-                invalidate();
-
-                default:
-                    return super.onTouchEvent(event);
+                spawnFigures(mRow, mCol, cellSize);
         }
+        return true;
 
+    }
 
+    private void spawnFigures(int row, int col, int cellSize) {
+        if (row >= 0 && row < cells && col >= 0 && col < cellSize) {
+            TicTacToeField.Figure figure = (mFigure == TicTacToeField.Figure.CROSS) ? TicTacToeField.Figure.CIRCLE : TicTacToeField.Figure.CROSS;
+            if (mTicTacToeField.setFigure(row, col, figure)) {
+                mFigure = figure;
+                isWin();
+            }
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    private boolean isWin() {
+        TicTacToeField.Figure win = mTicTacToeField.getWinner();
+
+        if (win.equals(TicTacToeField.Figure.NONE)) {
+            if (mTicTacToeField.isFull()) {
+                Toast.makeText(getContext(), "Ничья", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        } else {
+            mWinListener.onWin(win);
+            winnerMatrix = mTicTacToeField.getWinnerMatrix();
+            Toast.makeText(getContext(), "WIN: " + win, Toast.LENGTH_SHORT).show();
+            mGameStatus = GameStatus.GAME_OVER;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        size = Math.min(w, h );
+        left = (w - size) / 2;
+        top = (h - size) / 2;
+        right = size + left;
+        bottom = size + top;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int size = Math.min(getMeasuredHeight(), getMeasuredWidth());
+        setMeasuredDimension(size, size);
     }
 
     public void drawVerticalLine(Canvas canvas) {
-        canvas.drawLine(getWidth() * xRatio, 0f, getWidth() * xRatio, getHeight(), gridPaint);
-        canvas.drawLine(getWidth() * (2 * xRatio), 0f, getWidth() * (2 * xRatio), getHeight(), gridPaint);
+        canvas.drawLine(0f, 0f, 0f, (float)getHeight() * (4 * xRatio), gridPaint);
+        canvas.drawLine((float)getWidth() * (2 * xRatio), 0f, (float)getWidth() * (2 * xRatio), (float)getHeight() * (4 * xRatio), mPaint);
+        canvas.drawLine((float)getWidth() * xRatio , 0f, (float)getWidth()  * xRatio, (float)getHeight() * (4 * xRatio), mPaint);
+        canvas.drawLine(getWidth(), 0f, getWidth(), (float)getHeight() * (4 * xRatio), gridPaint);
     }
 
     public void drawHorizontalLine(Canvas canvas) {
-        canvas.drawLine(0f, getHeight() * xRatio, getWidth(), getHeight() * xRatio, gridPaint);
-        canvas.drawLine(0f, getHeight() * (2 * xRatio), getWidth(), getHeight() * (2 * xRatio), gridPaint);
-    }
 
-    public void drawX(Canvas canvas) {
-//        PointF pointF = new PointF(2, 2);
-        RectF rectF = new RectF(2, 2, 2, 2);
-        canvas.drawOval(rectF, gridPaint);
+        canvas.drawLine(0f, 0f, getWidth(), 0f, gridPaint);
+        canvas.drawLine(0f, (float) getHeight() / 2 * (2 * xRatio), getWidth(), (float) getHeight() / 2 * (2 * xRatio), mPaint);
+        canvas.drawLine(0f, (float) getHeight() / 2 * (4 * xRatio), getWidth(), (float) getHeight() / 2 * (4 * xRatio), mPaint);
+        canvas.drawLine(0f, (float) getHeight() * (3 * xRatio), getWidth(), (float) getHeight() * (3 * xRatio), gridPaint);
     }
 
 
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        SavedState state = new SavedState(super.onSaveInstanceState());
+        state.mTacToeField = mTicTacToeField;
+        state.figure = mFigure.ordinal();
+        return state;
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        super.onRestoreInstanceState(state);
+        SavedState myState = (SavedState) state;
+        mTicTacToeField = myState.mTacToeField;
+        mFigure = TicTacToeField.Figure.values()[myState.figure];
 
-
-
-
-    public void drawGrid(Canvas canvas) {
-        for (int i = 0; i < 2; i++) {
-            float left = alternativeWidth * (i + 1);
-
-            float right = left + tolzina;
-            float top = 0;
-            float bottom = height;
-
-            canvas.drawRect(left, top, right, bottom, gridPaint);
-
-
-            float left2 = 0;
-            float right2 = width;
-            float top2 = alternativeHeight * (i + 1);
-            float bottom2 = top2 * tolzina;
-
-            canvas.drawRect(left2, top2, right2, bottom2, gridPaint);
+        if (isWin()) {
+            mGameStatus = GameStatus.GAME_OVER;
+        } else {
+            mGameStatus = GameStatus.START;
         }
     }
+
+    private enum GameStatus {
+        START,
+        GAME,
+        GAME_OVER,
+        RESTART
+    }
+
+
+    private static class SavedState extends BaseSavedState {
+        private TicTacToeField mTacToeField;
+        private int figure;
+
+        public SavedState(Parcel source) {
+            super(source);
+            mTacToeField = source.readParcelable(getClass().getClassLoader());
+            figure = source.readInt();
+        }
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        public static final Creator<SavedState> CREATOR = new Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeParcelable(mTacToeField, 0);
+            out.writeInt(figure);
+        }
+    }
+
 }
